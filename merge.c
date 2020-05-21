de9532d9f039901ef8fe632dded46a6d9eacc166
root <jparodi@fi.uba.ar>
2020-05-21 18:59:30 +0000

    union c assembler

--- merge.c
+++ merge.c
@@ -1,19 +1,84 @@
 #include <stdio.h>
-#define SIZE 10
+#include <string.h>
+#include <stdlib.h>
 
 
-int main(){
-	int nums[SIZE] = {10,9,8,7,6,20,22,15,63,1};
+int* convertir_a_vector(char* cadena){
+	int len_cadena = 0;
+	for(int j = 0; (cadena[j] != '\0');j++){
+		if(cadena[j] == ' ')len_cadena++;
+	}
+	int* vector = (int*) malloc (sizeof(int)*(len_cadena+1));
+	const char sep[2] = " ";
+	char *temp;
+	//obtengo primer temp
+	temp = strtok(cadena, sep);
 
-	for (int i=0; i<SIZE; i++){
-		printf("Original:%d\n", nums[i]);
+	int i = 0;
+
+	while(temp != NULL){
+		vector[i] = atoi(temp);
+		i++;
+		temp = strtok(NULL, sep);
+	}
+	return vector;
+}
+
+void leer_archivo(const char* filename){
+
+    int len_linea = 0;
+
+    char c;
+
+    FILE * archivo = fopen (filename,"r");
+
+    while (c!=EOF){
+        while (c!=EOF){
+            c = fgetc(archivo);
+
+            if (c=='\n'){
+                fseek(archivo,-(len_linea)*sizeof(char),SEEK_CUR);
+                break;
+                }
+
+            len_linea++;
+        }
+
+        if (c!=EOF){
+            char linea [len_linea];
+            fgets(linea,len_linea,archivo);
+
+            int* vector = convertir_a_vector(linea);
+            for (int i=0;i<=3;i++){
+                printf("%i\n",vector[i]);
+            }
+            free(vector);
+
+            len_linea=0;
+            fseek(archivo,sizeof(char),SEEK_CUR);
+        }
+    }
+
+    printf("sali");
+    fclose(archivo);
+}
+
+
+int main(int argc, char *argv[]){
+
+	leer_archivo("prueba.txt");
+	int* vector_final;
+	int len_vector_final;
+
+	for (int i=0; i<len_vector_final; i++){
+		printf("Original:%d\n", vector_final[i]);
 	}
 
 	printf("-----------------------------------\n");
-	int* ptr = merge(nums, SIZE);
+	int* ptr = merge(vector_final, len_vector_final);
 
-	for (int i=0; i<SIZE; i++){
-		printf("Modificado:%d\n", nums[i]);
+	for (int i=0; i<len_vector_final; i++){
+		printf("Modificado:%d\n", vector_final[i]);
 	}
 
 	return 0;
