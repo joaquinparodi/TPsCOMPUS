c07832bd2e22ecdf21c561c48bcdae9c3a6c071e
root <jparodi@fi.uba.ar>
2020-05-21 00:51:17 +0000

    main arreglado

--- main.c
+++ main.c
@@ -2,38 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 int* convertir_a_vector(char* cadena){
-    int* vector = malloc (sizeof(int));
-    int len_vector = 0;
+	int len_cadena = 0;
+	for(int j = 0; (cadena[j] != '\0');j++){
+		if(cadena[j] == ' ')len_cadena++;
+	}
+	int* vector = (int*) malloc (sizeof(int)*(len_cadena+1));
+	const char sep[2] = " ";
+	char *temp;
+	//obtengo primer temp
+	temp = strtok(cadena, sep);
 
-    char* palabra = strtok(cadena, " ");
-
-    while( palabra != NULL ) {
-
-      vector[len_vector] = atoi(palabra);
-      palabra = strtok(NULL, " ");
-      len_vector ++;
-      vector= realloc(vector,len_vector * sizeof(int));
-    }
-
-    return vector;
+	int i = 0;
+	while(temp != NULL){
+		vector[i] = atoi(temp);
+		i++;
+		temp = strtok(NULL, sep);
+	}
+	return vector;
 }
 
 int main(int argc, char *argv[]){
-    int* vector= convertir_a_vector("1 2 3 4");
-    for (int i=0;i<=3;i++){
-        printf("%i\n",vector[i]);
-    }
-
-    for (int i=0;i<=3;i++){
-        free(&vector[i]);
-    }
-    return 0;
+	char cadena[7] = "1 2 3 4";
+	int* vector = convertir_a_vector(cadena);
+	for (int i=0;i<=3;i++){
+		printf("%i\n",vector[i]);
+	}
+	free(vector);
+	return 0;
 }
-
-
-
-
-
-
-
