ff29d505ceddd724da9b09c8576664b09f454d56
root <jparodi@fi.uba.ar>
2020-05-22 03:29:25 +0000

    todo listo, lo unico que falta es cubrir cuando mandan por stdin cat .txt

--- merge.c
+++ merge.c
@@ -11,7 +11,7 @@ int* convertir_a_vector(char* cadena){
             len_cadena++;
         }
 
-		else if( (isdigit(cadena[j])==0 && (cadena[j]!='-'))  || (cadena[j]=='-') && isdigit(cadena[j+1])==0){
+		else if( (isdigit(cadena[j])==0 && (cadena[j]!='-'))  || ((cadena[j]=='-') && isdigit(cadena[j+1])==0)){
             fprintf( stderr, "ERROR: Uno o más datos del archivo no son válidos, por ejemeplo el simbolo %c no se corresponde con un número.", cadena[j]);
             exit(EXIT_FAILURE);
         }
@@ -82,27 +82,54 @@ void leer_archivo(const char* filename){
                 }
         }
 
-        if (c!=EOF){
+        if (c!=EOF && len_linea>1){
             char linea [len_linea];
             fgets(linea,len_linea,archivo);
-            int* vector = convertir_a_vector(linea);
-            int len_vector=obtener_largo_vector(vector);
-            //merge(vector, len_vector);
-            imprimir_vector(vector,len_vector);
-            free(vector);
-        }
+            if (strcmp(linea,"\n") != 0){
+                int* vector = convertir_a_vector(linea);
+                int len_vector=obtener_largo_vector(vector);
+                merge(vector, len_vector);
+                imprimir_vector(vector,len_vector);
 
-        len_linea=0;
+                free(vector);
+            }
+            else{
+                printf("\n");
+            }
 
-        fseek(archivo,sizeof(char),SEEK_CUR);
+            len_linea=0;
+
+            fseek(archivo,sizeof(char),SEEK_CUR);
         }
+    }
 
     fclose(archivo);
 }
 
 int main(int argc, char *argv[]){
 
-	leer_archivo(argv[1]);
+	if(strcmp(argv[1],"-V") == 0){
+		printf("versión TP1 Organización de computadoras\n");
+		return 0;
+	}
+	if(strcmp(argv[1],"-h") == 0){
+		printf("Usage:\n");
+		printf("	tp1 -h\n");
+		printf("	tp1 -V\n");
+		printf("	tp1 -i in_file -o out_file\n");
+		printf("Options:\n");
+		printf("	-V, --version	Print version and quit.\n");
+		printf("	-h, --help	Print this information and quit.\n");
+		printf("	-i, --input	Specify input stream/file, '-' for stdin.\n");
+		printf("	-o, --output	Specify output stream/file, '-' for stdout.\n");
+		printf("Examples:\n");
+		printf("	tp1 < in.txt > out.txt\n");
+		printf("	cat in.txt | tp1 -i - > out.txt\n");
+		return 0;
+	}
+	if(strcmp(argv[1],"-i") == 0){
+		leer_archivo(argv[2]);
+	}
 
 	return 0;
 }
