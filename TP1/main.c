d52c51be37538432e3977185ef4100e4ff205875
root <jparodi@fi.uba.ar>
2020-06-02 04:27:23 +0000

    avance en main

--- TP1/main.c
+++ TP1/main.c
@@ -237,6 +237,14 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
+
+
+
+
+
+
+//NUEVO MAIN
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -311,6 +319,9 @@ void read_file(FILE* stream){
         get_numbers(line, vector);
         //ordenar
 
+        //merge del assembler
+        merge(vector, actualVectorLength);
+
         for (int i=0; i<actualVectorLength; i++){
             printf("%d ", vector[i]);
         }
@@ -319,44 +330,48 @@ void read_file(FILE* stream){
     printf("\n");
 }
 
+void menejo_de_error(char* mensaje){
+	fprintf(stderr, "%s \n", mensaje);
+    exit(EXIT_FAILURE);
+}
+
 
 int main(int argc, char* argv[]) {
-    //char* execOption = argv[1];
-    char* execOption = "-i";
 
-    //if (argc <= 2){
-    if (false){
-        if (strings_are_equal(execOption, HELP)){
+	FILE* in;
+
+	if(argv[1]){
+		char* execOption = argv[1];
+		if (strings_are_equal(execOption, HELP)){
             help();
         } else if (strings_are_equal(execOption, VERSION)){
-            printf("Version");
-        } else {
-            printf("No deberia estar aca");
-        }
-    } else {
-        //char* in_file = argv[2];
-//        char* out_command = argv[3];
-//        char* out_file = argv[4];
-
-        char* in_file = "entrada.txt";
-        char* out_command = "-o";
-        char* out_file = "salida.txt";
-        
-
-        FILE* in;
-        if (!strings_are_equal(in_file, STD)){
+          	printf("versión TP1 Organización de computadoras\n");
+        } else if (strings_are_equal(execOption, INPUT)){
+        	if(!argv[2])
+        		manejo_de_error("falta el archivo de texto");
+        	char* in_file = argv[2];
             in = fopen(in_file, "r+");
-        } else {
-            in = stdin;
+        } else{
+        	printf("no es valido", );
         }
-
         read_file(in);
+        char* out_command = argv[3];
+        if(strings_are_equal(execOption, out_command)){
+        	if (!argv[4])
+                manejo_de_error("archivo output invalido\n");
+        	char* out_file = argv[4];
+        	if (!strings_are_equal(out_file, STD)){
+         	   printf("Salida por archivo");
+    		}
+    	}
 
-        if (!strings_are_equal(out_file, STD)){
-            printf("Salida por archivo");
-        }
-    }
+    //stdin
+	}else{
+		in = stdin;
+		read_file(in);
+	}
 
-    return 0;
+	return 0;
 }
+
 
